c8badfe4373b399681dfc48d97895b44f0c460fc
nima.taheri@hootsuite.com <nima.taheri@hootsuite.com>
2024-02-04 16:15:35 -0800

    feat: add simplerest

---
+++ cmd/simplerest.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	ginTimeout "github.com/gin-contrib/timeout"
+	gin "github.com/gin-gonic/gin"
+	"net/http"
+	"time"
+)
+
+type EchoResponse struct {
+	FieldOne string `json:"fieldOne"`
+	FieldTwo int64  `json:"fieldTwo"`
+}
+
+func hello(c *gin.Context) {
+	// intentional delay of 3 seconds
+	time.Sleep(3 * time.Second)
+
+	// do not continue if cancelled
+	if !c.IsAborted() {
+		data := EchoResponse{
+			FieldOne: "string-value",
+			FieldTwo: 1000,
+		}
+		c.JSON(http.StatusOK, data)
+
+		// to test after timeout whether we could skip request processing
+		println("Not aborted.")
+	}
+}
+
+func main() {
+	router := gin.Default()
+	router.Use(ginTimeout.New(
+		ginTimeout.WithTimeout(2*time.Second),
+		ginTimeout.WithHandler(func(context *gin.Context) {
+			context.Next()
+		}),
+	))
+	router.GET("/echo", hello)
+	err := router.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
+}
